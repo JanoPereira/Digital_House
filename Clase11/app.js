43df0bed01610aa3a8e7cfb59d907a2f74f1befd
Jano <janoo.pereira@gmail.com>
2022-06-21 23:43:08 -0300

    clase 11

--- Clase11/app.js
+++ Clase11/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.static('public'));
+
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,"./views/home.html"))
 })
 
 app.listen(3000,()=>{
-    
+    console.log("Se ha iniciado el servidor en http://localhost:3000")
 })
